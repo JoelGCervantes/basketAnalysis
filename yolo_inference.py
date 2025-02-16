a87eed5b8de2cbbe31360b9c4d16248b1d6f9410
Joel <garciacervantesj00@gmail.com>
2025-02-16 15:44:00 -0800

    messing with yolo (video prediction)

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,2 +1,12 @@
 from ultralytics import YOLO
 
+model = YOLO('yolov10m')
+
+results = model.predict('input_videos/RPReplay_Final1739697445.mp4', save=True)
+print(results[0])
+
+print('======== printing bx in boxes =========')
+for box in results[0].boxes:
+    print(box)
+
+
