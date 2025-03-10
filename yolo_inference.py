3d528180c512a0d08f6de0d1f4cac14c0fd46a0c
Joel <garciacervantesj00@gmail.com>
2025-03-10 13:19:41 -0700

    .py file update

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 
+# use yolov10m pretrained model for object detection on video and print some results
 model = YOLO('yolov10m')
 
 results = model.predict('input_videos/RPReplay_Final1739697445.mp4', save=True)
